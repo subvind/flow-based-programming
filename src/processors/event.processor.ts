b36d027ff228b1909359b88b7a3dc35212c0116c
Travis Burandt <travis.burandt@gmail.com>
2024-09-04 19:26:25 -0500

    multiple connections

--- src/processors/event.processor.ts
+++ src/processors/event.processor.ts
@@ -5,7 +5,7 @@ import { ComponentRegistry } from '../services/component-registry.service';
 @Injectable()
 export class EventProcessor {
   private readonly logger = new Logger(EventProcessor.name);
-  private connections: Map<string, { toFlow: string; toComponent: string; toEvent: string }> = new Map();
+  private connections: Map<string, Array<{ toFlow: string; toComponent: string; toEvent: string }>> = new Map();
 
   constructor(private componentRegistry: ComponentRegistry) {}
 
@@ -23,10 +23,10 @@ export class EventProcessor {
       this.logger.log(`Passing event to component: ${componentId}`);
       await component.handleEvent(eventId, eventData);
 
-      // Check if there's a connection for this event
+      // Check if there are connections for this event
       const connectionKey = `${flowId}.${componentId}.${eventId}`;
-      const connection = this.connections.get(connectionKey);
-      if (connection) {
+      const connections = this.connections.get(connectionKey) || [];
+      for (const connection of connections) {
         const { toFlow, toComponent, toEvent } = connection;
         this.logger.log(`[forwardingComponentEvent] [${toFlow}.${toComponent}.${toEvent}]`);
         
@@ -53,7 +53,10 @@ export class EventProcessor {
     this.logger.log(`Received createConnection: ${flowId}.${fromComponent}.${fromEvent} -> ${toComponent}.${toEvent}`);
     
     const connectionKey = `${flowId}.${fromComponent}.${fromEvent}`;
-    this.connections.set(connectionKey, { toFlow: flowId, toComponent, toEvent });
+    if (!this.connections.has(connectionKey)) {
+      this.connections.set(connectionKey, []);
+    }
+    this.connections.get(connectionKey).push({ toFlow: flowId, toComponent, toEvent });
     this.logger.log(`Connection created: ${connectionKey} -> ${toComponent}.${toEvent}`);
   }
 }
